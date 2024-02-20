15ff77204f4f9a22fb4cebfdcfcef6fbb38d205c
meyersa <88554476+meyersa@users.noreply.github.com>
2024-02-20 10:26:47 -0500

    Changing display of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,34 @@ import Layout from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
-import Author from '../components/author' 
+import Author from '../components/author'
+import Image from 'next/image'
 
 export default function Home({ allPostsData }) {
   return (
     <Layout>
       <div className="content">
-        <h2>Blog</h2>
+        <h2>Welcome to my Blog!</h2>
+        <p>I'm August. I'm currently pursuing a double major in Information Technology and Computer Science with a minor in Math at Central Michigan University. While here, I'm working as a Student Technician for the Office of Information Technology helping support Faculty and Staff with their technology. In my free time I'm either experimenting in my lab learning something new, out exploring the world with my camera, or playing video games (if I have time)</p>
+        <div className="divider" />
+      </div>
+      <div className="content">
+        <h2 >Latest Posts</h2>
+      </div>
         <ul>
-          {allPostsData.map(({ id, date, title, except, author, authorImg }) => (
-            <li key={id}>
-              <Link href={`${id}`}>{title}</Link>
-              <br />
-              <Date dateString={date} />
-              <Author author={{author, authorImg}}/>
-            </li>
+          {allPostsData.map(({ id, date, title, exerpt, author, authorImg, image }, index) => (
+            <div className="content">
+              <li key={id}>
+                <Link href={`${id}`}>{title}</Link>
+                <br />
+                <Date dateString={date} />
+                <Image src={image} alt="Cover image" width={500} height={500} style={{ width: '100%', height: '10rem' }} />
+                <p>{exerpt}</p>
+                {index !== allPostsData.length - 1 && <div className="divider" />}
+              </li>
+            </div>
           ))}
         </ul>
-      </div>
     </Layout>
   );
 }
