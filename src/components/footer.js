67798921eb4a809d915e22ba7dd718e0655bf296
meyersa <88554476+meyersa@users.noreply.github.com>
2024-02-20 13:40:29 -0500

    redundant footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,6 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <div className={styles.container}>
-      <Link href="/"><p>writeups.meyersa.com</p></Link>
       <p>&copy; 2024 August Meyers</p>
     </div>
   );
